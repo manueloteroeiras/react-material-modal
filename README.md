1e5722ed9ac4675b84d7f6caf3c791a988d3de45
manuel <manuel.otero@line64.com>
2017-01-26 21:33:40 -0300

    modal refactor

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# Modal: reactJS modal component
-Simple modal to reactJS
+# React Modal Component
 
-## Introduction
+### Start: 
+``` npm install react-material-modal --save ```
+
+### How to use react Modal
+```
+ <Modal
+    size="md"
+    title="Modal Header"
+    show={ this.state.showing }
+    close={ ()=> this.setState({ showing : false }) }
+    headerStyle={{ color: '#fff', backgroundColor: '#00796b' }}
+    bodyStyle={{ color: '#000', backgroundColor: '#26a69a' }}>
+    
+    <div style={{ textAlign: 'center' }}>
+        <p>Modal Body</p>
+    </div>
+    
+</Modal>
+```
+Enjoy it!
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react';
-
+import Fontawesome from 'react-fontawesome';
 
 import styles from './styles';
 
@@ -9,17 +9,54 @@ export default class Modal extends Component {
     
   }
 
-  renderModal(){
-    return(
-      <div style={ styles.containerModal }>
-        <div>
-          <h5>{ this.props.title }</h5>
-        </div>
+
+  renderHeader(){
+    let styleProps = this.props.headerStyle || {};
+    return (
+      <div style={{ ...styles.modal.header, ...styleProps }}>
+        { (this.props.title) ? <h5 style={ styles.modal.title }>{ this.props.title }</h5> : null} 
+        <Fontawesome name="times" onClick={ () => this.props.close() } />
       </div>
     )
   }
+
+  renderBody(){
+    let styleProps = this.props.bodyStyle || {};
+    return(
+      <div style={{ ...styles.modal.body, ...styleProps }}>
+        { this.props.children }
+      </div>
+    )
+  }
+
+  renderModal(){
+    return(
+      <div style={ styles.containerModal }>
+          <div style={{ ...styles.modal, ...{ width: this.getModalSize() } }}>
+            { this.renderHeader() }
+            { this.renderBody() }
+          </div>
+      </div>
+    )
+  }
+
+  getModalSize(){
+    let size = '20vh';
+    switch(this.props.size){
+      case 'sm' : 
+        size = '50vh';
+        break;
+      case 'md' : 
+        size = '80vh';
+        break;
+      case 'lg' : 
+        size = '120vh';
+        break
+    }
+    return size
+  }
   
   render(){
-    (this.props.showing) ? this.renderModal() : null
+    return (this.props.showing) ? this.renderModal() : null
   }
 }
